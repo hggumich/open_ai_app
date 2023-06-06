0523da2e3191be23a201dbb470dfdb422c5585ef
Hector Garza <hggumich@gmail.com>
2023-06-06 19:40:13 -0400

    refactor to an API backend

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const readline = require('readline')
+const express = require('express')
 const { generateMeta, generateImage } = require('./controllers/openaiController')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+// app setup
+const app = express()
+app.listen(4000, () => console.log('listening for requests on port 4000'))
 
-//rl.question('YouTube Video Title: \n', generateMeta)
-rl.question('Describe your YouTube thumbnail: \n', generateImage)
+// middleware
+app.use(express.json())
+
+// routes
+app.post('/openai/meta', generateMeta)
+app.post('/openai/image', generateImage)
